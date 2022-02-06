ca3bafd6b9a37bc624c7fbf050426f7e5d946163
James Hoang <jhoang2002@gmail.com>
2022-02-06 01:16:12 -0800

    Started Projectile again

--- RaccoonGame/src/Projectile.java
+++ RaccoonGame/src/Projectile.java
@@ -1,6 +1,8 @@
 import java.awt.Rectangle;
 import com.engine.core.gfx.*;
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Projectile extends Interactable{
     //Tracks who owns the projectile. Enemy or player
@@ -59,4 +61,223 @@ public class Projectile extends Interactable{
             spriteColor = Color.red;
         }
     }
+
+    /// <summary>
+    /// Returns if the projectile is alive
+    /// </summary>
+    /// <returns>A boolean</returns>
+    public boolean GetIsAlive()
+    {
+        //Returns if the projectile is alive
+        return isAlive;
+    }
+
+    /// <summary>
+    /// Returns if the projectile is friendly to the player
+    /// </summary>
+    /// <returns>A boolean</returns>
+    public boolean GetIsFriendly()
+    {
+        return isFriendly;
+    }
+
+    /// <summary>
+    /// Updates the projectile object for player
+    /// </summary>
+    public void Update(List<Character>[] enemy)
+    {
+        //Calls CheckCollisions
+        CheckCollisions(enemy);
+
+        //Moves in on enemies if homing
+        if (isHoming)
+        {
+            //Sets a target if one did not exist
+            if(target == null)
+            {
+                //Calls CalcTarget
+                CalcTarget(enemy);
+            }
+
+            //Calls CalcMoveDir
+            CalcMoveDir();
+
+            //Calls movement
+            move();
+        }
+
+        //Calls UpdatePosition
+        UpdatePosition();
+    }
+
+    /// <summary>
+    /// Updates the projectile object for enemies
+    /// </summary>
+    public void Update(Character enemy)
+    {
+        //Calls CheckCollisions
+        CheckCollisions(enemy);
+
+        //Moves in on enemies if homing
+        if (isHoming)
+        {
+            //Sets the enemy as the target
+            target = enemy;
+
+            //Calls CalcMoveDir
+            CalcMoveDir();
+        }
+
+        //Calls UpdatePosition
+        UpdatePosition();
+    }
+
+    /// <summary>
+    /// Updates the position of the projectile
+    /// </summary>
+    @Override
+    public void UpdatePosition()
+    {
+        //Updates the distance traveled
+        distTraveled += currentVelocity.Length() * Data.deltaTime;
+
+        //Removes the projectile once it has reached its maximum range
+        if(distTraveled > stat[Data.RANGE])
+        {
+            //Sets the projectile to be deleted
+            isAlive = false;
+        }
+
+        base.UpdatePosition();
+    }
+
+    /// <summary>
+    /// Calaculates the nearest enemy and homes in on it
+    /// </summary>
+    /// <param name="enemy">A list of arrays of type character</param>
+    public void CalcMoveDir()
+    {
+        ///Changes the velocity if there are targets
+        if(target != null)
+        {
+            //Sets the acceleration towards the target
+            accel.X = target.GetDestRec().Center.X - destRec.Center.X;
+            accel.Y = target.GetDestRec().Center.Y - destRec.Center.Y;
+
+            //Changes the accceleration values to a direction vector
+            if (accel.X != 0 && accel.Y != 0)
+            {
+                //Normalizes the vector
+                accel.Normalize();
+
+                //Scales the direction by the acceleration value
+                accel *= maxAccel;
+            }
+            else
+            {
+                //There is no acceleration
+                accel *= 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Calculates the closes enemy and sets it as the target
+    /// </summary>
+    /// <param name="enemy"></param>
+    public void CalcTarget(List<Character>[] enemy)
+    {
+        //Tracks the distance to target
+        int distToTargetSqred = 1000000;
+
+        //Loops for all enemies types
+        for (int i = 0; i < enemy.Length; i++)
+        {
+            //Loops for all spesific enemy
+            for (int j = 0; j < enemy[i].Count; j++)
+            {
+                //Compares the old closest distance to the distance to a new character
+                if (Data.GetDistSqred(enemy[i][j].GetDestRec().Center, destRec.Center) < distToTargetSqred)
+                {
+                    //Sets the new distance as the current enemies distance
+                    distToTargetSqred = Data.GetDistSqred(enemy[i][j].GetDestRec().Center, destRec.Center);
+
+                    //Sets the target to the current enemy
+                    target = enemy[i][j];
+                }
+            }
+        }
+    }
+
+     /// <summary>
+    /// Checks collisions to determine if the projectile should be deleted
+    /// </summary>
+    public void CheckCollisions(Character enemy)
+    {
+
+        //Checks if the projectile has collided with the current enemy
+        if (Data.IsCollided(enemy.GetDestRec(), this.destRec))
+        {
+            //Sets the projectile to be deleted
+            isAlive = false;
+
+            //Hits the enemy by the projectiles damage
+            enemy.Hit(stat[Data.DMG], currentVelocity, destRec.Width);
+            enemy.SetIsDirty(true);
+        }
+
+        //Calls CheckCollBoundry
+        CheckCollBoundry();
+    }
+
+    /// <summary>
+    /// Checks collisions to determine if the projectile should be deleted
+    /// </summary>
+    public void CheckCollisions(List<Character>[] enemy)
+    {
+        //Loops for all enemies types
+        for (int i = 0; i < enemy.Length; i++)
+        {
+            //Loops for all spesific enemy
+            for(int j = 0; j < enemy[i].Count; j++)
+            {
+                //Checks if the projectile has collided with the current enemy
+                if (Data.IsCollided(enemy[i][j].GetDestRec(), this.destRec))
+                {
+                    //Sets the projectile to be deleted
+                    isAlive = false;
+
+                    //Hits the enemy by the projectiles damage
+                    enemy[i][j].Hit(stat[Data.DMG], currentVelocity, destRec.Width);
+                    enemy[i][j].SetIsDirty(true);
+                }
+            }
+        }
+
+        //Calls CheckCollBoundry
+        CheckCollBoundry();
+    }
+
+    /// <summary>
+    /// Checks for collions againts the room boundry
+    /// </summary>
+    public void CheckCollBoundry()
+    {
+        //Checks if the projetile is beyond the room boundries
+        if (destRec.Top < Data.roomBoundary.Top || destRec.Right > Data.roomBoundary.Right ||
+            destRec.Bottom > Data.roomBoundary.Bottom || destRec.Left < Data.roomBoundary.Left)
+        {
+            isAlive = false;
+        }
+    }
+
+    /// <summary>
+    /// Draws the projectile
+    /// </summary>
+    /// <param name="spriteBatch"></param>
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        base.Draw(spriteBatch);
+    }
+
 }
